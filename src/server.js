09011f6d5c9ef434a1a45ca217ecf8c52b77c00e
ana-aranha <anab.aranha@gmail.com>
2022-09-30 16:19:22 -0300

    adding categories routers

--- src/server.js
+++ src/server.js
@@ -1,6 +1,6 @@
 import express from "express";
 import cors from "cors";
-import connection from "./database/database.js";
+import categoriesRouter from "./routers/categoriesRouter.js";
 
 const app = express();
 app.use(express.json());
@@ -10,11 +10,7 @@ app.get("/status", (req, res) => {
 	res.send("Ok");
 });
 
-app.get("/categories", (req, res) => {
-	connection.query("SELECT * FROM categories").then((categories) => {
-		res.send(categories.rows);
-	});
-});
+app.use(categoriesRouter);
 
 app.listen(4000, () => {
 	console.log("Server listening on port 4000.");
