5884fdb1f8b75ed9fa06bb9fe9b0861e6f5b4a87
ana-aranha <anab.aranha@gmail.com>
2022-10-03 17:06:07 -0300

    Changing rental schema to accept only integers

--- src/schemas/rentalsSchema.js
+++ src/schemas/rentalsSchema.js
@@ -1,9 +1,9 @@
 import joi from "joi";
 
 const rentalSchema = joi.object({
-	customerId: joi.number().required(),
-	gameId: joi.number().required(),
-	daysRented: joi.number().required().min(1),
+	customerId: joi.number().required().min(1).integer(),
+	gameId: joi.number().required().min(1).integer(),
+	daysRented: joi.number().required().min(1).integer(),
 });
 
 export default rentalSchema;
